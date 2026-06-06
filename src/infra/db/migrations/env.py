c1cdf2ca9599f47db9143da7f4b363e3f91c54eb
kani0dev <allanchagasilva@gmail.com>
2026-06-05 22:26:24 -0300

    creat firs migration with alambic

--- src/infra/db/migrations/env.py
+++ src/infra/db/migrations/env.py
@@ -1,6 +1,6 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import create_engine
 from sqlalchemy import pool
 
 from alembic import context
@@ -38,7 +38,12 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+
+    from dotenv import load_dotenv
+    load_dotenv()
+    from os import getenv
+
+    url = config.get_main_option("sqlalchemy.url",f"mysql+pymysql://{getenv('DB_USER')}:{getenv('DB_PASS')}@{getenv('HOST')}:{getenv('DB_PORT')}/{getenv('DB_NAME')}")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -51,23 +56,17 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+    from dotenv import load_dotenv
+    load_dotenv()
+    from os import getenv
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    url = f"mysql+pymysql://{getenv('DB_USER')}:{getenv('DB_PASS')}@{getenv('HOST')}:{getenv('DB_PORT')}/{getenv('DB_NAME')}"
+    connectable = create_engine(url, poolclass=pool.NullPool)
 
     with connectable.connect() as connection:
         context.configure(
             connection=connection, target_metadata=target_metadata
         )
-
         with context.begin_transaction():
             context.run_migrations()
 
